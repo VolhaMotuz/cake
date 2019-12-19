b60bd8944b71b068afc3e02424264551ae7133d8
olga.motuz <olga.motuz@coxautoinc.com>
2019-12-19 18:17:46 +0300

    fixed footer

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let navButton = document.getElementsByClassName('navigation-button');
-let navList = document.getElementsByClassName('navigation-list');
+let navList = document.getElementsByClassName('header__navigation-list');
 
 navButton[0].addEventListener('click', function() {
     navList[0].classList.toggle('navigation-list_active');
